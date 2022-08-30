2de4036cf3dde030857bbe7a3454f4e7457326b4
WMs784 <64195816+WMs784@users.noreply.github.com>
2022-08-31 02:03:36 +0900

    Update header.js

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Header() {
     return (<>
     <div className="header">
-        <Image src="/tabigacha_logo.svg" className="headerImage"/>
+         {/* <Image src="/tabigacha_logo.svg" className="headerImage"/> */}
     </div>
         </>
     );
